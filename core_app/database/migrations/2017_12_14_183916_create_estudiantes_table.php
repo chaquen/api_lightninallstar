a28427bf4e25103513b4bf1e001b4c09f63f61a2
chaquen <edgar.guzman21@gmail.com>
2017-12-14 13:45:21 -0500

    se creaan modelos, migraciones y controladores
    se creaan modelos, migraciones y controladores

---
+++ core_app/database/migrations/2017_12_14_183916_create_estudiantes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstudiantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('estudiantes');
+    }
+}
---
+++ core_app/database/migrations/2017_12_14_184001_create_detalle_estudiante_grupos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetalleEstudianteGruposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_estudiante_grupos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('detalle_estudiante_grupos');
+    }
+}
